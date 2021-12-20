88088cdce5d9d178992a53aa86c47a766e4ba633
Parker Wahle <parkeredwardwahle2017@gmail.com>
2021-12-20 13:12:37 -0500

    Initial Commit

---
+++ CelesteTestMod/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Celeste.Mod;
+
+namespace CelesteTestMod
+{
+    public class CelesteTestModule : EverestModule
+    {
+        public static CelesteTestModule Instance;
+
+        public CelesteTestModule()
+        {
+            Instance = this;
+        }
+
+        public override void Load()
+        {
+        }
+
+        public override void Unload()
+        {   
+        }
+    }
+}
